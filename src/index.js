c1460b1e7eb4f842731cb1cfbebc71b9ec29e7fd
Perry <petreli1@ymail.com>
2022-01-11 15:11:50 +0000

    fixed?

--- src/index.js
+++ src/index.js
@@ -37,7 +37,6 @@ switchUnits.addEventListener('click' ,() => {
 async function getForecast(location, units) {
         const data = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + location + '&units=' + units + '&APPID=d1cad75804f6bd996f5d83905ac66876', {mode: 'cors'})
         data.json().then(function(response) {
-            console.log(response.list);
             updateForecast(response.list, units);
         });
 }
